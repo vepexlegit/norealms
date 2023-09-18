b3aec032a06fd233d75c8ae77707e5263b1079f7
vepexlegit <88085309+vepexlegit@users.noreply.github.com>
2023-09-18 19:59:24 +0400

    Update NoRealms.java

--- src/main/java/de/vepexlegit/norealms/NoRealms.java
+++ src/main/java/de/vepexlegit/norealms/NoRealms.java
@@ -8,50 +8,60 @@ import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
+import org.apache.logging.log4j.Logger;
 
-@Mod(modid = NoRealms.MODID, version = NoRealms.VERSION)
+@Mod(modid = NoRealms.MODID, name = NoRealms.NAME, version = NoRealms.VERSION)
 public class NoRealms {
     public static final String MODID = "norealms";
+    public static final String NAME = "NoRealms";
     public static final String VERSION = "1.0";
 
     public NoRealms() {
         MinecraftForge.EVENT_BUS.register(this);
     }
-    
+
+    private static Logger logger;
+
+    @EventHandler
+    public void preInit(FMLPreInitializationEvent event) {
+        logger = event.getModLog();
+    }
+
     @EventHandler
     public void init(FMLInitializationEvent event) {
-        System.out.println("DIRT BLOCK >> " + Blocks.dirt.getUnlocalizedName());
+        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
     }
 
     @SubscribeEvent
     public void onGuiMainMenuInit(GuiScreenEvent.InitGuiEvent.Post event) {
-        if (event.gui instanceof GuiMainMenu) {
-            for (GuiButton button : event.buttonList) {
+        if (event.getGui() instanceof GuiMainMenu) {
+            for (GuiButton button : event.getButtonList()) {
                 if (button.id == 14) {
-                    event.buttonList.remove(button);
+                    event.getButtonList().remove(button);
                     break;
                 }
             }
         }
-        if (event.gui instanceof GuiMainMenu) {
+        if (event.getGui() instanceof GuiMainMenu) {
             GuiButton modsButton = null;
-            for (GuiButton button : event.buttonList) {
+            for (GuiButton button : event.getButtonList()) {
                 if (button.id == 6) {
                     modsButton = button;
                     break;
                 }
             }
             if (modsButton != null) {
-                event.buttonList.remove(modsButton);
+                event.getButtonList().remove(modsButton);
 
                 int x = modsButton.width / 2 + 242;
                 int y = modsButton.height + 170;
 
-                modsButton.xPosition = x;
-                modsButton.yPosition = y;
+                modsButton.x = x;
+                modsButton.y = y;
 
-                event.buttonList.add(modsButton);
+                event.getButtonList().add(modsButton);
             }
         }
     }
